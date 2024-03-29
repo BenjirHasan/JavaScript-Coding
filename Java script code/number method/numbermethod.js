fef3944796998a2d013c6d6845618ab068f920f3
MD. BENJIR HASAN <149584348+BenjirHasan@users.noreply.github.com>
2024-03-29 11:13:18 +0600

    Add files via upload

---
+++ Java script code/number method/numbermethod.js
@@ -0,0 +1,15 @@
+//TYPE 1
+var num = 20;
+console.log(typeof(num)) ;
+
+//TYPE 2
+var num = "20.5";
+num = parseFloat(num); //for float number
+
+console.log(typeof(num));
+
+//TYPE 3
+var num = 2.5678;
+
+console.log(number.toFixed(2)); //how many number should display after point
+console.log(number.toPrecision(3));  // how many number should display overall
---
+++ Java script code/output writting/test.js
@@ -0,0 +1,2 @@
+//console.log('HELLO DRACULA');
+//document.write('DRACULA');
